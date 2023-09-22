5ee47243d8f267c2c02165a4250c4a46c86222cf
Ufanie <aniboy188@gmail.com>
2023-09-22 21:45:59 +0100

    yes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Your App Name</p>
+      <p>&copy; {new Date().getFullYear()} Image Gallery</p>
       <p>
         Designed by <a href="https://github.com/ufaniee">Anietie Joseph</a>
       </p>
--- src/components/ImageDropZone.js
+++ src/components/ImageDropZone.js
@@ -12,6 +12,7 @@ const ImageGallery = () => {
     "https://media.istockphoto.com/id/1333977253/photo/male-lion-resting-on-a-rock.webp?b=1&s=170667a&w=0&k=20&c=RtsqEhhNPcCFz3GF8PzddrBbL7s1Sr-r3W9hGP9h9yg=",
     "https://media.istockphoto.com/id/1472824601/photo/majestic-wild-horses-in-autumn-in-wyoming.webp?b=1&s=170667a&w=0&k=20&c=8-aXrw531-iWyrfviwur2WHxXA9vKMMhNXWP6qAonhc=",
     "https://media.istockphoto.com/id/1435255379/photo/marine-iguana-on-a-sandy-beach-on-espanola-island-galapagos-islands-ecuador.webp?b=1&s=170667a&w=0&k=20&c=0xNvLxShc9WVpywkLNm5nIhpegcanWFmqmp3qIMMfRw=",
+    "https://media.istockphoto.com/id/1435255379/photo/marine-iguana-on-a-sandy-beach-on-espanola-island-galapagos-islands-ecuador.webp?b=1&s=170667a&w=0&k=20&c=0xNvLxShc9WVpywkLNm5nIhpegcanWFmqmp3qIMMfRw=","https://images.unsplash.com/photo-1567633090480-f19f2f67c088?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d2lsZCUyMGFuaW1hbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60","https://images.unsplash.com/photo-1623645481161-0d8160281cbf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d2lsZCUyMGFuaW1hbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
     "https://media.istockphoto.com/id/1458215547/photo/brown-bear.webp?b=1&s=170667a&w=0&k=20&c=1ADJtvC9wQFy1Arglt8YEDLy-rqU2ZNAYHTongfTJJU=",
    "https://media.istockphoto.com/id/1399799270/photo/extreme-close-up-of-great-white-shark-looking-directly-at-camera-smiling.webp?b=1&s=170667a&w=0&k=20&c=rN6jdN4og6v1UBlYIR3fgg2IjprUeVbKnTyHdiHdGiI=",
     "https://media.istockphoto.com/id/1333977253/photo/male-lion-resting-on-a-rock.webp?b=1&s=170667a&w=0&k=20&c=RtsqEhhNPcCFz3GF8PzddrBbL7s1Sr-r3W9hGP9h9yg=",
@@ -20,6 +21,7 @@ const ImageGallery = () => {
     "https://media.istockphoto.com/id/1458215547/photo/brown-bear.webp?b=1&s=170667a&w=0&k=20&c=1ADJtvC9wQFy1Arglt8YEDLy-rqU2ZNAYHTongfTJJU=",
    "https://media.istockphoto.com/id/1399799270/photo/extreme-close-up-of-great-white-shark-looking-directly-at-camera-smiling.webp?b=1&s=170667a&w=0&k=20&c=rN6jdN4og6v1UBlYIR3fgg2IjprUeVbKnTyHdiHdGiI=",
     "https://media.istockphoto.com/id/1333977253/photo/male-lion-resting-on-a-rock.webp?b=1&s=170667a&w=0&k=20&c=RtsqEhhNPcCFz3GF8PzddrBbL7s1Sr-r3W9hGP9h9yg=",
+    "https://media.istockphoto.com/id/1435255379/photo/marine-iguana-on-a-sandy-beach-on-espanola-island-galapagos-islands-ecuador.webp?b=1&s=170667a&w=0&k=20&c=0xNvLxShc9WVpywkLNm5nIhpegcanWFmqmp3qIMMfRw=","https://images.unsplash.com/photo-1567633090480-f19f2f67c088?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d2lsZCUyMGFuaW1hbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60","https://images.unsplash.com/photo-1623645481161-0d8160281cbf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d2lsZCUyMGFuaW1hbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
     "https://media.istockphoto.com/id/1472824601/photo/majestic-wild-horses-in-autumn-in-wyoming.webp?b=1&s=170667a&w=0&k=20&c=8-aXrw531-iWyrfviwur2WHxXA9vKMMhNXWP6qAonhc=",
     "https://media.istockphoto.com/id/1435255379/photo/marine-iguana-on-a-sandy-beach-on-espanola-island-galapagos-islands-ecuador.webp?b=1&s=170667a&w=0&k=20&c=0xNvLxShc9WVpywkLNm5nIhpegcanWFmqmp3qIMMfRw=",
     "https://media.istockphoto.com/id/1458215547/photo/brown-bear.webp?b=1&s=170667a&w=0&k=20&c=1ADJtvC9wQFy1Arglt8YEDLy-rqU2ZNAYHTongfTJJU=",
@@ -31,8 +33,8 @@ const ImageGallery = () => {
     "https://media.istockphoto.com/id/1399799270/photo/extreme-close-up-of-great-white-shark-looking-directly-at-camera-smiling.webp?b=1&s=170667a&w=0&k=20&c=rN6jdN4og6v1UBlYIR3fgg2IjprUeVbKnTyHdiHdGiI=",
      "https://media.istockphoto.com/id/1333977253/photo/male-lion-resting-on-a-rock.webp?b=1&s=170667a&w=0&k=20&c=RtsqEhhNPcCFz3GF8PzddrBbL7s1Sr-r3W9hGP9h9yg=",
      "https://media.istockphoto.com/id/1472824601/photo/majestic-wild-horses-in-autumn-in-wyoming.webp?b=1&s=170667a&w=0&k=20&c=8-aXrw531-iWyrfviwur2WHxXA9vKMMhNXWP6qAonhc=",
-     "https://media.istockphoto.com/id/1435255379/photo/marine-iguana-on-a-sandy-beach-on-espanola-island-galapagos-islands-ecuador.webp?b=1&s=170667a&w=0&k=20&c=0xNvLxShc9WVpywkLNm5nIhpegcanWFmqmp3qIMMfRw="
- 
+     "https://media.istockphoto.com/id/1435255379/photo/marine-iguana-on-a-sandy-beach-on-espanola-island-galapagos-islands-ecuador.webp?b=1&s=170667a&w=0&k=20&c=0xNvLxShc9WVpywkLNm5nIhpegcanWFmqmp3qIMMfRw=","https://images.unsplash.com/photo-1567633090480-f19f2f67c088?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d2lsZCUyMGFuaW1hbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60","https://images.unsplash.com/photo-1623645481161-0d8160281cbf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d2lsZCUyMGFuaW1hbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1557008075-7f2c5efa4cfd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8d2lsZCUyMGFuaW1hbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
 ]);
 
   const moveImage = (fromIndex, toIndex) => {
